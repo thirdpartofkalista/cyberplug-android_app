0bcf1360882bacafaa07f5003ec73b51dcca27d3
cezarmathe <cezarmathe@gmail.com>
2018-07-17 13:26:57 +0300

    Finished the basic networking

--- app/src/main/java/com/criss/cyberplug/networking/HttpRequestsHandler.java
+++ app/src/main/java/com/criss/cyberplug/networking/HttpRequestsHandler.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.criss.cyberplug.R;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -26,9 +27,15 @@ public class HttpRequestsHandler {
         }
     }
 
-    private HttpURLConnection getConnection() throws IOException {
+//    Method that returns an HTTP URL Connection
+//    Passing 0 returns a POST connection
+//    Passing 1 returns a GET connection
+    private HttpURLConnection getConnection(boolean x) throws IOException {
         HttpURLConnection conn = (HttpURLConnection) serverURL.openConnection();
-        conn.setRequestMethod("POST");
+        if (x)
+            conn.setRequestMethod("POST");
+        else
+            conn.setRequestMethod("GET");
 //        conn.setRequestProperty("Content-Type", "application/json");
 //        conn.setRequestProperty("Accept","application/json");
         conn.setDoOutput(true);
@@ -37,17 +44,46 @@ public class HttpRequestsHandler {
     }
 
 //    Main methods
-    public void sendGet(Object json) {
+
+//    Method that sends a json as a GET request
+    public void sendGet(final String json) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    HttpURLConnection conn = getConnection(false);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(json);
+                    os.flush();
+                    os.close();
+                    conn.disconnect();
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
         thread.start();
     }
 
-    public void sendPost(Object json) {
+//    Method that sends a json as a POST request
+    public void sendPost(final String json) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpURLConnection conn = getConnection(true);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(json);
+                    os.flush();
+                    os.close();
+                    conn.disconnect();
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
