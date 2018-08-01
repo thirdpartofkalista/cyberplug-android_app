0f5ea6a346c449b9bda5dbec3ae6afa816259dbb
cezarmathe <cezarmathe@gmail.com>
2018-08-01 18:48:23 +0300

    worked on the main activity

--- app/src/main/java/com/criss/cyberplug/MainActivity.java
+++ app/src/main/java/com/criss/cyberplug/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mainPage;
 
+    private boolean shouldReloadList = false;
+
+    private int lastPlace = 0; // 0->devices, 1->groups
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,23 +23,51 @@ public class MainActivity extends AppCompatActivity {
 
         mainPage = findViewById(R.id.drawer_layout);
 
+        reloadList();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
+
+                        if (menuItem.getItemId() == R.id.all_devices_button) {
+                            lastPlace = 0;
+                            menuItem.setChecked(true);
+                            shouldReloadList = true;
+                        }
+                        else if (menuItem.getItemId() == R.id.groups_button) {
+                            lastPlace = 1;
+                            menuItem.setChecked(true);
+                            shouldReloadList = true;
+                        }
+                        else {
+
+                        }
+
                         mainPage.closeDrawers();
 
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
+                        if (shouldReloadList)
+                            reloadList();
 
                         return true;
                     }
                 });
 
     }
+
+    private void reloadList() {
+        if (lastPlace == 0) {
+//            do something
+        }
+
+        else if (lastPlace == 1) {
+//            do smth else
+        }
+
+        shouldReloadList = false;
+    }
+
+
 }
 
