7bc9ba474c9589bd12d3c18bbd933f71123dcb11
cezarmathe <cezarmathe@gmail.com>
2018-08-04 12:22:02 +0300

    almost working demo interface

--- app/src/main/java/com/criss/cyberplug/MainActivity.java
+++ app/src/main/java/com/criss/cyberplug/MainActivity.java
@@ -1,5 +1,6 @@
 package com.criss.cyberplug;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,8 +9,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.criss.cyberplug.list_adapters.DeviceListAdapter;
 import com.criss.cyberplug.list_adapters.GroupListAdapter;
@@ -22,8 +25,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-
-    private boolean shouldReloadList = false;
 
     private int lastPlace = 0; // 0->devices, 1->groups
 
@@ -43,6 +44,69 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton addButton;
 
+    private NavigationView navigationView;
+
+
+//    Handle the interaction with the drawer menu items
+    private NavigationView.OnNavigationItemSelectedListener drawerOnNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            Toast.makeText(getApplicationContext(), "works", Toast.LENGTH_LONG).show();
+
+            int id = menuItem.getItemId();
+
+            switch (id) {
+                case R.id.all_devices_button:
+                    lastPlace = 0;
+                    reloadList();
+                    menuItem.setChecked(true);
+                    break;
+
+                case R.id.groups_button:
+                    lastPlace = 1;
+                    reloadList();
+                    menuItem.setChecked(true);
+                    break;
+
+                case R.id.about_button:
+                    break;
+
+                case R.id.help_button:
+                    break;
+
+                case R.id.log_out_button:
+                    break;
+
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            return true;
+        }
+    };
+
+//    Handle the interaction with the floating button
+    private View.OnClickListener addButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            devices.add(new Device(0, "device 0", false, true));
+            Toast.makeText(getApplicationContext(), "works", Toast.LENGTH_LONG).show();
+            reloadList();
+        }
+    };
+
+    //    Handle the interaction with the action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void reloadList() {
 
@@ -53,27 +117,19 @@ public class MainActivity extends AppCompatActivity {
         else if (lastPlace == 1) {
             list.setAdapter(groupListAdapter);
         }
-
-        shouldReloadList = false;
     }
 
     private void initializeList() {
 
+        devices = new ArrayList<>();
+
+        groups = new ArrayList<>();
+
         deviceListAdapter = new DeviceListAdapter(devices, getApplicationContext());
 
         groupListAdapter = new GroupListAdapter(groups, getApplicationContext());
-    }
 
-
-//    Handle the interaction with the action bar
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+//        reloadList();
     }
 
 
@@ -96,54 +152,15 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         list = findViewById(R.id.main_list);
         addButton = findViewById(R.id.add_button);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
+        addButton = findViewById(R.id.add_button);
 
 //        Initialize the list
         initializeList();
-        reloadList();
 
-//        Handle the interaction with the drawer
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                        if (menuItem.getItemId() == R.id.all_devices_button) {
-                            if (lastPlace != 0)
-                                shouldReloadList = true;
-                            lastPlace = 0;
-                            menuItem.setChecked(true);
-                        }
-                        else if (menuItem.getItemId() == R.id.groups_button) {
-                            if (lastPlace != 1)
-                                shouldReloadList = true;
-                            lastPlace = 1;
-                            menuItem.setChecked(true);
-                        }
-                        else {
-                            switch (menuItem.getItemId()) {
-                                case R.id.settings_button:
-                                    break;
-
-                                case R.id.about_button:
-                                    break;
-
-                                case R.id.help_button:
-                                    break;
-
-                                case R.id.log_out_button:
-                                    break;
-                            }
-                        }
-
-                        drawerLayout.closeDrawers();
-
-                        if (shouldReloadList)
-                            reloadList();
-
-                        return true;
-                    }
-                });
+//        Set event listeners
+        navigationView.setNavigationItemSelectedListener(drawerOnNavigationItemSelectedListener);
+        addButton.setOnClickListener(addButtonOnClickListener);
 
     }
 
