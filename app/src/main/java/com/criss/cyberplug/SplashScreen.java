80635c52ef5167ce184e6a7b3f26f6204a604942
thirdpartofkalista <dreams.fulfilled@yahoo.com>
2018-08-16 15:15:32 +0300

    colors

--- app/src/main/java/com/criss/cyberplug/SplashScreen.java
+++ app/src/main/java/com/criss/cyberplug/SplashScreen.java
@@ -9,7 +9,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen);
 
         Intent intent = new Intent(this, Landing.class);
         startActivity(intent);
