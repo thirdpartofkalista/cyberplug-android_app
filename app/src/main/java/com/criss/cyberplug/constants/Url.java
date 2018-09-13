25daa8a0e8c950d5e083eefe64f5476ab7f9948c
cezarmathe <cezarmathe@gmail.com>
2018-09-13 05:23:30 -0400

    changed url port

--- app/src/main/java/com/criss/cyberplug/constants/Url.java
+++ app/src/main/java/com/criss/cyberplug/constants/Url.java
@@ -2,6 +2,6 @@ package com.criss.cyberplug.constants;
 
 public class Url {
 
-    public static final String serverUrl = "http://78.96.238.161:1234/";
+    public static final String serverUrl = "http://78.96.238.161:1000/";
 
 }
